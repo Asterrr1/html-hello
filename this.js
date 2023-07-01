f075196eb3893891e55417006d2380cf3fd8ad10
Asterrr1 <125838534+Asterrr1@users.noreply.github.com>
2023-07-01 04:01:14 +0000

    commit

--- this.js
+++
@@ -1,7 +0,0 @@
-let openCreatePostPrompt = () => {
-    document.getElementById("NewPost").style.display = "flex";
-};
-
-let closeCreatePostPrompt = () => {
-    document.getElementById("NewPost").style.display = "none";
-};
